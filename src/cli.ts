10bd1b4cd5528037d62b41308ceafde3fa2ce5e2
Bruno Rodrigues <brunocroh@gmail.com>
2024-02-17 20:34:24 -0300

    fix: optition version now is returning version from package.json

--- src/cli.ts
+++ src/cli.ts
@@ -2,17 +2,23 @@
 import { execa } from "execa";
 import NotifyMe from "./index.js";
 import { Command } from "commander";
+const PackageJson = require("../package.json");
 const program = new Command();
 
 program
   .description("Notify me when my tasks are finished")
-  .version("1.0.0", "-v, --version", "display the version number")
+  .option("-v, --version", "display the version number")
   .option("-t, --title <string>")
   .option("-d, --description <string>")
   .option("-c, --command <string>")
   .option("-a, --arguments [arguments...]")
   .option("-s, --sound")
   .action(async (opts) => {
+    if (opts.version) {
+      console.log(PackageJson.version);
+      return;
+    }
+
     if (opts.command) {
       const result = await execa(opts.command, opts.arguments);
 
