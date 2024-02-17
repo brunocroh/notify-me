93153ef2b1d2607ab3ad882d6be858a673b8d67e
Bruno Rodrigues <brunocroh@gmail.com>
2024-02-17 19:22:56 -0300

    fix: add esbuild plugins to tsup to use bundle on build

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from "tsup";
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
+import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
 
 export default defineConfig({
+  esbuildPlugins: [
+    NodeModulesPolyfillPlugin(),
+    NodeGlobalsPolyfillPlugin({
+      process: true,
+      buffer: true,
+    }),
+  ],
   clean: true,
   dts: true,
-  bundle: false,
+  bundle: true,
   entry: ["src/cli.ts", "src/index.ts"],
   format: ["esm"],
   sourcemap: true,
