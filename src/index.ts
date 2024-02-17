ae5ee04026bd18c1e2786f89de1a6d5e76e014b9
Bruno Rodrigues <brunocroh@gmail.com>
2024-02-17 11:47:30 -0300

    feat: add execa and display a simple mock notification

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("works?");
+import { execa } from "execa";
+
+type RunProps = {
+  title: string;
+  description: string;
+};
+
+const DEFAULT_DESCRIPTION = "Your task finished";
+const DEFAULT_TITLE = "Done";
+
+const run = async (
+  title: string = DEFAULT_TITLE,
+  description: string = DEFAULT_DESCRIPTION,
+) => {
+  await execa(`osascript`, [
+    `-e display notification "${description}" with title "${title}"`,
+  ]);
+};
+
+run();
